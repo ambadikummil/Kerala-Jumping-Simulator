8996e5f1933391142d1953dea8eebd6b3718f8ad
AMBADI R <ambadikummil6@gmail.com>
2026-06-18 12:40:50 +0530

    Initial file upload
    Initial project Upload

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GroundGenerator : MonoBehaviour
 {
     public Transform groundPoint;
+    PUBLIC Transform minHeightPoint;
 
     public ObjectPooler[] groundPoolers;
 
