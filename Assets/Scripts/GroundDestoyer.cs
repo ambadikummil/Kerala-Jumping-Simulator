280a27e9a0ce9d269d72f97a78455690d0a0465b
AMBADI R <ambadikummil6@gmail.com>
2026-06-18 12:36:29 +0530

    Initial commit

---
+++ Assets/Scripts/GroundDestoyer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDestoyer : MonoBehaviour
+
+{
+    private GameObject point;
+
+    void Start()
+    {
+        point = GameObject.Find("GroundEndPoint");
+    }
+
+    void Update()
+    {
+        if(transform.position.x < point.transform.position.x)
+        {
+            gameObject.SetActive(false);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    public LayerMask ground;
+    private Rigidbody2D rigidbody;
+    private Collider2D playerCollider;
+    private Animator animator;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+
+        bool grounded = Physics2D.IsTouchingLayers(playerCollider, ground);
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if(grounded)
+                {
+                    rigidbody.velocity = new Vector2(rigidbody.velocity.x, jump);
+                }
+
+        }
+        animator.SetBool("Grounded", grounded);
+    }
+}
